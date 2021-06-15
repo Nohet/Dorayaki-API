a3aaae62f4465a9b06164c3563e602028978f3ca
Nohet <70505658+Nohet@users.noreply.github.com>
2021-06-15 21:00:44 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
 function btoa(str) {
-    let buffer;
+    let buffer
   
     if (str instanceof Buffer) {
-      buffer = str;
+      buffer = str
     } else {
-      buffer = Buffer.from(str.toString(), "binary");
+      buffer = Buffer.from(str.toString(), "binary")
     }
-    return buffer.toString("base64");
+    return buffer.toString("base64")
   }
   
 function linkvertise(userid, link) {
-    let base_url = `https://link-to.net/${userid}/${Math.random() * 1000}/dynamic`;
-    let href = base_url + "?r=" + btoa(encodeURI(link));
-    return href;
+    let base_url = `https://link-to.net/${userid}/${Math.random() * 1000}/dynamic`
+    let href = base_url + "?r=" + btoa(encodeURI(link))
+    return href
   }
 
 module.exports = {
